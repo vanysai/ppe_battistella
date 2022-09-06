730e59950d3db1aea66dd0fd2034057212cd7a1a
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-09-06 16:16:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const calcular = function() {
+    let cateto1 = parseFloat(document.getElementById('cateto1').value)/1; 
+    let cateto2 = parseFloat(document.getElementById('cateto2').value)/1;
+    let hipotenusa = document.getElementById('hipotenusa')
+    
+    hipotenusa.value = Math.pow(cateto1, 2) + Math.pow(cateto2, 2) ; 
+    alert(Math.sqrt(hipotenusa.value));
+    document.getElementById('hipotenusa').value 
+}
+
+const excluir = function(){
+    document.getElementById('cateto1').value = ""
+    document.getElementById('cateto2').value = ""
+    document.getElementById('hipotenusa').value = ""
+}
+
+
+// Battistella, para o alert aparecer é preciso clicar duas vezes no button calcular, desculpa por esse bug :(
